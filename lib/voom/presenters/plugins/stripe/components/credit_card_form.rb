f8e155ed228fda14f05edd8af61ae4d92b2c19c5
Derek Graham <derek@geotix.com>
2020-06-23 22:19:36 -0400

    Take an optional submit button ID and bind the confirmCardPayment action to that, if present.

--- lib/voom/presenters/plugins/stripe/components/credit_card_form.rb
+++ lib/voom/presenters/plugins/stripe/components/credit_card_form.rb
@@ -7,13 +7,14 @@ module Voom
         module Components
           class CreditCardForm < DSL::Components::EventBase
 
-            attr_reader :stripe_publishable_key, :client_secret, :payment_intent_id, :tag
+            attr_reader :stripe_publishable_key, :client_secret, :payment_intent_id, :tag, :submit_id
 
             def initialize(stripe_publishable_key, client_secret, payment_intent_id, **attribs, &block)
               @stripe_publishable_key = stripe_publishable_key
               @client_secret = client_secret
               @payment_intent_id = payment_intent_id
-              @tag = attribs.fetch(:tag, nil)
+              @submit_id = attribs.delete(:submit_id) { 'validate-card' }
+              @tag = attribs.delete(:tag) { nil }
               super(type: :stripe_credit_card_form, **attribs, &block)
               expand!
             end
