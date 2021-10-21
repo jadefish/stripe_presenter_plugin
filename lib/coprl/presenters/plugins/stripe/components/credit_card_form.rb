7dc353912c0deb413db76d282b51c4e30f5da753
Nick Miller <nick@evvnt.com>
2021-10-21 10:33:06 -0400

    Add stripe_payment_request_form component

--- lib/coprl/presenters/plugins/stripe/components/credit_card_form.rb
+++ lib/coprl/presenters/plugins/stripe/components/credit_card_form.rb
@@ -7,14 +7,19 @@ module Coprl
         module Components
           class CreditCardForm < DSL::Components::EventBase
 
-            attr_reader :stripe_publishable_key, :address_config, :currency, :form_type
+            attr_reader :stripe_publishable_key,
+                        :address_config,
+                        :currency,
+                        :form_type
 
             def initialize(stripe_publishable_key, **attribs, &block)
               @stripe_publishable_key = stripe_publishable_key
               @address_config = attribs.delete(:address_config){ {} }
               @currency = attribs.delete(:currency){ 'usd' }
               @form_type = attribs[:form_type] || :multi_line
+
               super(type: :stripe_credit_card_form, **attribs, &block)
+
               expand!
             end
 
